c66931216ae46ec7cf85639ee32e6b16e6c926b6
Matthew Brown <lokislayer@gmail.com>
2018-08-07 15:10:32 -0500

    Sanity check added
    Added a check to make sure THERE are dice to be rolled.

--- main.py
+++ main.py
@@ -22,13 +22,28 @@ purple = inputNumberOrReturn(question4)
 yellow = inputNumberOrReturn(question5)
 red = inputNumberOrReturn(question6)
 
-dice.append(blue)
-dice.append(black)
-dice.append(green)
-dice.append(purple)
-dice.append(yellow)
-dice.append(red)
+# CHECK TO SEE IF THERE ARE DICE TO BE ROLLED OR NOT
+while True:
+    if (blue + black + green + purple + yellow + red == 0):
+        print("Error : No dice have been used. I need at least one to begin")
+        print("")
+        blue = inputNumberOrReturn(question1)
+        black = inputNumberOrReturn(question2)
+        green = inputNumberOrReturn(question3)
+        purple = inputNumberOrReturn(question4)
+        yellow = inputNumberOrReturn(question5)
+        red = inputNumberOrReturn(question6)
+        continue
+    else:
+        dice.append(blue)
+        dice.append(black)
+        dice.append(green)
+        dice.append(purple)
+        dice.append(yellow)
+        dice.append(red)
+        break
 
 GenRolled = GenesysDiceRoller(dice)
 GenRolled.displayResults()
+
 #end of main area
