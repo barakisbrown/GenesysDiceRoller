8b715430ac6a03e002bd18dd194207aeb0532f31
Matthew Brown <lokislayer@gmail.com>
2019-02-15 13:47:21 -0600

    actually entered info needed.

---
+++ questions.py
@@ -0,0 +1,8 @@
+# STATIC STRINGS
+about = "Genesys Python Dice Roller by Matthew Brown, @2018"
+question1 = "How many boost dice? (return for 0) => "
+question2 = "How many setback dice (return for 0) => "
+question3 = "How many Ability dice (return for 0) => "
+question4 = "How many Difficulty dice (return for 0) => "
+question5 = "How many Proficiency dice (return for 0) => "
+question6 = "How many Challenge dice (return for 0) => "
