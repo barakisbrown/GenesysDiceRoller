f1af89141742c3d5daa7fbf649dc29ce9ef1c2b9
Matthew Brown <lokislayer@gmail.com>
2019-02-15 13:47:34 -0600

    removed what was needed

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from utils import inputNumberOrReturn
 from Gensysroller import GenesysDiceRoller
+from questions import *
 
-# STATIC STRINGS
-about = "Genesys Python Dice Roller by Matthew Brown, @2018"
-question1 = "How many boost dice? (return for 0) => "
-question2 = "How many setback dice (return for 0) => "
-question3 = "How many Ability dice (return for 0) => "
-question4 = "How many Difficulty dice (return for 0) => "
-question5 = "How many Proficiency dice (return for 0) => "
-question6 = "How many Challenge dice (return for 0) => "
+
 
 # MAIN PART OF PROGRAM HERE
 dice = []
